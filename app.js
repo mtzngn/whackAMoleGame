c692dafe5923ea3f72e1bcb86180d1739d215947
mtzngn <mt.zngn@gmail.com>
2020-12-17 17:50:15 +0000

    buttons added and reset button code created

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const moles = document.getElementsByClassName("mole")
 let score = 0;
 
+document.getElementById("start").addEventListener("click", ()=>{
 
-
+})
 window.setInterval(function(){
 
     let i = Math.floor(Math.random() * 6) ;
@@ -17,5 +18,10 @@ for (let j = 0; j < moles.length; j++) {
         document.getElementById("score").textContent  = score;
         moles[j].style.backgroundImage = "url('./assets/mole-hit.png')"
         setTimeout(function() {moles[j].style.backgroundImage = "url('./assets/mole.png')"}, 600);    
+        
     } )
 }
+document.getElementById("reset").addEventListener("click", ()=>{
+    score = 0;
+    document.getElementById("score").textContent = score;
+})
