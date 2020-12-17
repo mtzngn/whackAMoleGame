936734158c18f2c946c93e49cbbcc4c13d3d8131
mtzngn <mt.zngn@gmail.com>
2020-12-17 17:58:05 +0000

    app.js tidy up

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ let score = 0;
 
 document.getElementById("start").addEventListener("click", ()=>{
     window.setInterval(function(){
-
         let i = Math.floor(Math.random() * 6) ;
         moles[i].style.transform = "translateY(0px)";
-        setTimeout(function() {moles[i].style.transform = "translateY(80px)"}, 1000);    
-      }, 1000);
-})
-for (let j = 0; j < moles.length; j++) {
-    moles[j].addEventListener("click", ()=>{
-        score++;
-        document.getElementById("score").textContent  = score;
-        moles[j].style.backgroundImage = "url('./assets/mole-hit.png')"
-        setTimeout(function() {moles[j].style.backgroundImage = "url('./assets/mole.png')"}, 600);    
+        setTimeout(function() {moles[i].style.transform = "translateY(80px)"}, 800);}, 800);
         
-    } )
-}
-document.getElementById("reset").addEventListener("click", ()=>{
-    score = 0;
-    document.getElementById("score").textContent = score;
+      for (let j = 0; j < moles.length; j++) {
+        moles[j].addEventListener("click", ()=>{
+            score++;
+            document.getElementById("score").textContent  = score;
+            moles[j].style.backgroundImage = "url('./assets/mole-hit.png')"
+            setTimeout(function() {moles[j].style.backgroundImage = "url('./assets/mole.png')"}, 600);    
+            
+        } )
+    }
+    document.getElementById("reset").addEventListener("click", ()=>{
+        score = 0;
+        document.getElementById("score").textContent = score;
+    })
+    
 })
