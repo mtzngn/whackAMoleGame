c7536167c02015706cf79bd6e70e75cacd66fa37
mtzngn <mt.zngn@gmail.com>
2020-12-17 16:40:25 +0000

    score calculating with the clciking to the moles added to the code

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const moles = document.getElementsByClassName("mole")
-
+let score = 0;
 
 window.setInterval(function(){
 
     let i = Math.floor(Math.random() * 6) ;
     moles[i].style.transform = "translateY(0px)";
-    setTimeout(function() {moles[i].style.transform = "translateY(80px)"}, 1000);
-
-    
+    setTimeout(function() {moles[i].style.transform = "translateY(80px)"}, 1000);    
   }, 1000);
 
 
+for (let j = 0; j < moles.length; j++) {
+    moles[j].addEventListener("click", ()=>{
+        score++;
+        document.getElementById("score").textContent  = score;
+    } )
+}
