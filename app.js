90450a88ec8eee2cf36cba145c34383a715b085e
mtzngn <mt.zngn@gmail.com>
2020-12-17 17:52:07 +0000

    code for start button added

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const moles = document.getElementsByClassName("mole")
 let score = 0;
 
 document.getElementById("start").addEventListener("click", ()=>{
+    window.setInterval(function(){
 
+        let i = Math.floor(Math.random() * 6) ;
+        moles[i].style.transform = "translateY(0px)";
+        setTimeout(function() {moles[i].style.transform = "translateY(80px)"}, 1000);    
+      }, 1000);
 })
-window.setInterval(function(){
-
-    let i = Math.floor(Math.random() * 6) ;
-    moles[i].style.transform = "translateY(0px)";
-    setTimeout(function() {moles[i].style.transform = "translateY(80px)"}, 1000);    
-  }, 1000);
-
-
 for (let j = 0; j < moles.length; j++) {
     moles[j].addEventListener("click", ()=>{
         score++;
